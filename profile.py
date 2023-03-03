9f4c195659768dcb24d3b46d8f08b592af157914
Bakhtawar Bhutto <Bakhtawar.bhutto56@gmail.com>
2023-03-02 22:48:01 -0800

    second day

---
+++ profile.py
@@ -0,0 +1,15 @@
+print("                                                          my           profile")
+name="Bakhtawar"
+last_name="Bhutto"
+age="20"
+institute_name="NCRG IST"
+batch="second"
+department="Remote Sensing And Geo Informatics"
+semester="fourth"
+print("name:",name)
+print("last_name:",last_name)
+print("age:",age)
+print("institute_name:",institute_name)
+print("batch:",name)
+print("department:",department)
+print("semester:",semester)
