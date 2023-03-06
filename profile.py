0bb8b15a5988c89bb0d92fefafe4a3a32b6de58b
Bakhtawar56 <bakhtawar.bhutto56@gmail.com>
2023-03-06 00:24:56 -0800

    changes saved

--- profile.py
+++ profile.py
@@ -1,4 +1,4 @@
-print("                                                          my           profile")
+print("\tMy Profile")
 name="Bakhtawar"
 last_name="Bhutto"
 age="20"
@@ -10,6 +10,6 @@ print("name:",name)
 print("last_name:",last_name)
 print("age:",age)
 print("institute_name:",institute_name)
-print("batch:",name)
+print("batch:",batch)
 print("department:",department)
 print("semester:",semester)
