a9afac7f969d608b142c55a8bbba3213b55e17c5
Bakhtawar56 <bakhtawar.bhutto56@gmail.com>
2023-03-29 23:42:27 -0700

    fruits

---
+++ tuple.py
@@ -0,0 +1,6 @@
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
+print("The last fruit is ",last_fruit)
